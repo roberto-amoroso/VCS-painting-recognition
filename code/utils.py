8640bfa9f62b9137c623e9aa7f2a6e356bddb9c2
roberto-amoroso <roberto.amoroso96@gmail.com>
2020-06-18 23:06:11 -0500

    [CODE] -fixed minor bugs and managed the case when a painting is found more times in the same frame
    - for the duplicated painting, I try to find again the painting with a lower `max_distance`

--- code/utils.py
+++ code/utils.py
@@ -85,13 +85,13 @@ def order_points(pts):
     # bottom-left, and the fourth is the bottom-right
     rect = np.zeros((4, 2))  # dtype="float32"
     # the top-left point will have the smallest sum, whereas
-    # the bottom-right will have the largest difference
+    # the bottom-right will have the largest sum
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
     # now, compute the difference between the points, the
     # top-right point will have the smallest difference,
-    # whereas the bottom-left point will have the largest sum
+    # whereas the bottom-left point will have the largest difference
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
